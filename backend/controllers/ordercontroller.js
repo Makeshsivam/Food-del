b0c5bd3389a0672e945b42f8c5fe8db501421e71
Makeshsivam <makesh.p.2005@gmail.com>
2025-10-04 23:31:23 +0530

    Update ordercontroller.js

--- backend/controllers/ordercontroller.js
+++ backend/controllers/ordercontroller.js
@@ -5,7 +5,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeorder = async (req, res) => {
-  const frontend_url = "http://localhost:5174";
+  const frontend_url = "https://food-del-frontend-3sr5.onrender.com";
 
   try {
     // create new order with userId from middleware
